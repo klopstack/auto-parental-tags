704da8b8973e78758793510ecc55918e5b021104
Ben <benklop@gmail.com>
2025-12-29 05:12:59 -0500

    Remove logging from ServiceRegistrator and update coverage information

--- Jellyfin.Plugin.AutoParentalTags/ServiceRegistrator.cs
+++ Jellyfin.Plugin.AutoParentalTags/ServiceRegistrator.cs
@@ -2,7 +2,6 @@ using Jellyfin.Plugin.AutoParentalTags.Services;
 using MediaBrowser.Controller;
 using MediaBrowser.Controller.Plugins;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.AutoParentalTags;
 
@@ -14,16 +13,8 @@ public class ServiceRegistrator : IPluginServiceRegistrator
     /// <inheritdoc />
     public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
     {
-        // Try to get logger factory, fallback to null logger if not available (e.g., in tests)
-        var loggerFactory = applicationHost.Resolve<ILoggerFactory>();
-        var logger = loggerFactory?.CreateLogger<ServiceRegistrator>();
-
-        logger?.LogInformation("Registering Auto Parental Tags services");
-
         serviceCollection.AddSingleton<AiServiceFactory>();
         serviceCollection.AddSingleton<LibraryMonitor>();
         serviceCollection.AddSingleton<AutoParentalTagsScheduledTask>();
-
-        logger?.LogDebug("Services registered: AiServiceFactory, LibraryMonitor, AutoParentalTagsScheduledTask");
     }
 }
